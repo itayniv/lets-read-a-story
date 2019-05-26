6531f03b351733e2db582a3e041e3a0a9e59c520
Itay Niv <itayniv@gmail.com>
2019-05-25 20:53:56 -0400

    https

--- app.js
+++ app.js
@@ -23,12 +23,27 @@ const userID = 0;
 
 let model;
 
+
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+
+
+
 const app = express();
 let server = http.createServer(app);
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(requireHTTPS);
+
 
 
 
